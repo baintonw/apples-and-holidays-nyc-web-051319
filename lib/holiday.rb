75e34420d273006fe788378749eac5034325053e
William Bainton <baintonw@gmail.com>
2019-05-07 14:51:04 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,17 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  
+  holiday_hash.each do |season, holidays|
+    puts "#{season.to_s.capitalize!}:"
+    holidays.each do |holiday, supplies|
+      puts "#{holiday.capitalize}"
 
+      supplies.each do |supply|
+          puts "#{supply.join}"
+      end
+    end  
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
